9a89d9772d025580851d6e724b3d01bba3d4bdd7
Timo Kock <timokock@gmail.com>
2021-10-13 20:51:33 +0200

    Implemented Users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    
-    constructor(props) {
-        super();
-        this.state = {
-            id: 'id',
-            login: 'mojombo',
-            avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/mojombo'
-        }
-    }
 
     render() {
-        const { login, avatar_url, html_url } = this.state
+        const { login, avatar_url, html_url } = this.props.user
         return (
             <div className="card text-center">
                 <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
