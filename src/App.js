6babd452c134b2ca052e7dcbaaa95740f64fda84
Timo Kock <timokock@gmail.com>
2021-10-13 19:53:26 +0200

    Implemented Navbar, defaultProps & propTypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/layout/Navbar'
+import './App.css'
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
