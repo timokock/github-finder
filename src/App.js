576b2fe245ef964621abee223972df6135535a1c
Timo Kock <timo.kock@movicoders.com>
2021-10-14 09:36:57 +0200

    Implemented Seach Users API request

--- src/App.js
+++ src/App.js
@@ -11,12 +11,21 @@ class App extends Component {
     loading: false
   }
 
-  async componentDidMount() {
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    
+  //   this.setState({ users: response.data, loading: false })
+  // }
+
+  searchUsers = async (text) => {
     this.setState({ loading: true });
 
-    const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    const response = await axios.get(`https://api.github.com/search/users?q=${text}&?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    console.log(response);
     
-    this.setState({ users: response.data, loading: false })
+    this.setState({ users: response.data.items, loading: false })
   }
 
   render() {
@@ -24,7 +33,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers}/>
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export class Search extends Component {
     state = {
@@ -11,7 +12,8 @@ export class Search extends Component {
 
     onSubmit = e => {
         e.preventDefault()
-        console.log(this.state.text);
+        this.props.searchUsers(this.state.text)
+        this.setState({ text: '' })
     }
 
     render() {
@@ -24,6 +26,10 @@ export class Search extends Component {
             </div>
         )
     }
+}
+
+Search.propTypes = {
+    searchUsers : PropTypes.func.isRequired,
 }
 
 export default Search
