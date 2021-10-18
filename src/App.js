9d8163f09a83307069a57d259649de302ee6f9c1
Timo Kock <timokock@gmail.com>
2021-10-18 08:51:02 +0200

    Refactored Alert, App component & added Home component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
 import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
+import { Home } from './components/pages/Home'
 import User from './components/users/User'
-import Search from './components/users/Search'
 import About from './components/pages/About'
 import Alert from './components/layout/Alert'
 import './css/App.css'
-
-
 
 const App = () => {
 
   return (
     <GithubState>
-    <AlertState>
-    <Router>
-    <div className="App">
-      <Navbar />
-      <div className="container">
-      <Alert />
-        <Switch>
-          <Route exact path='/' render={props => (
-            <Fragment>
-              <Search />
-              <Users />
-            </Fragment>
-          )} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/user/:login' render={props => (
-            <User {...props}/>               
-          )} />
-        </Switch>
-      </div>
-    </div>
-    </Router>
-    </AlertState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+              <div className="container">
+              <Alert />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/user/:login' component={User} />
+                </Switch>
+              </div>
+          </div>
+        </Router>
+      </AlertState>
     </GithubState>
   )
 }
